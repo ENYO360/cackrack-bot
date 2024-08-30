a5240255fa11a81afac205aabd6a24fc28b31455
Shaibu Moses <106714361+ENYO360@users.noreply.github.com>
2024-08-30 04:36:20 +0100

    The Nav and Hero section are now live

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <>
+            <nav>
+                <a href="#"><h2>CACKCRACK BOT</h2></a>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
